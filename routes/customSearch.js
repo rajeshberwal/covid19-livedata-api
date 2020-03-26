b9a8d1942d44c970c5dd921e7e3c3325db716791
Rajesh Berwal <rajesh.umedberwal@gmail.com>
2020-03-26 19:49:26 +0530

    data updated

--- routes/customSearch.js
+++ routes/customSearch.js
@@ -5,7 +5,7 @@ const data = require('../data/live-data.json')
 
 /* GET users listing. */
 router.get('/:country_name', function(req, res, next) {
-    const country = req.params.country_name;
+    const country = req.params.country_name.toLowerCase();
     const totalCases = data.TotalCases[country];
     const totalDeaths = data.TotalDeaths[country];
     const totalReacovered = data.TotalRecovered[country];
